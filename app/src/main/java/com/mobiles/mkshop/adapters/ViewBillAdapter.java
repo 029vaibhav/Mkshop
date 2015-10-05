537a43652d020b7e7151f9689d070b03fce3c9fc
vaibhav <vaibs4007@rediff.com>
2015-10-06 01:20:32 +0530

    bill viewing started

--- app/src/main/java/com/mobiles/mkshop/adapters/ViewBillAdapter.java
+++ app/src/main/java/com/mobiles/mkshop/adapters/ViewBillAdapter.java
@@ -1,18 +1,23 @@
 package com.mobiles.mkshop.adapters;
 
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.mobiles.mkshop.R;
 import com.mobiles.mkshop.pojos.ProductExpense;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -23,10 +28,25 @@ public class ViewBillAdapter extends RecyclerView.Adapter<ViewBillAdapter.ViewHo
 
     List<ProductExpense> productExpenseList;
     Fragment context;
+    Dialog dialog;
+    View view;
+    ImageView imageView, backButton;
+    RecyclerView recyclerView;
 
     public ViewBillAdapter(Fragment userListFragment, List<ProductExpense> productExpenseList) {
         this.context = userListFragment;
         this.productExpenseList = productExpenseList;
+
+        dialog = new Dialog(context.getActivity(), R.style.AppTheme);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.view_bill_dialog);
+
+
+        imageView = (ImageView) dialog.findViewById(R.id.bill_image);
+        backButton = (ImageView) dialog.findViewById(R.id.back_button);
+        recyclerView = (RecyclerView) dialog.findViewById(R.id.recycler_view);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(context.getActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);
 
     }
 
@@ -86,15 +106,33 @@ public class ViewBillAdapter extends RecyclerView.Adapter<ViewBillAdapter.ViewHo
         public void onClick(View v) {
 
             new MaterialDialog.Builder(context.getActivity())
-                    .items(R.array.userActions)
+                    .items(R.array.bill_action)
                     .itemsCallback(new MaterialDialog.ListCallback() {
                         @Override
                         public void onSelection(MaterialDialog materialDialog, View view, int i, CharSequence charSequence) {
 
                             switch (i) {
                                 case 0:
+
+
+                                    ViewBillDialogAdapter viewBillDialogAdapter = new ViewBillDialogAdapter(context, productExpenseList.get(getAdapterPosition()).getProductExpenseSingleEntries());
+                                    recyclerView.setAdapter(viewBillDialogAdapter);
+                                    materialDialog.dismiss();
+                                    backButton.setOnClickListener(new View.OnClickListener() {
+                                                                      @Override
+                                                                      public void onClick(View v) {
+                                                                          dialog.dismiss();
+                                                                      }
+                                                                  }
+                                    );
+                                    dialog.show();
+                                    Picasso.with(context.getActivity()).load(productExpenseList.get(getAdapterPosition()).getImage()).into(imageView);
+
+
                                     break;
                                 case 1:
+
+
                                     break;
                             }
 
